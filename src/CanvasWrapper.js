bdbd4ded8e863d8a34709b5b7ee42a9d3fbe5714
Kristin <kristin.clark042@gmail.com>
2019-05-01 10:57:28 -0600

    move update and drawing of stars to their own file

--- src/CanvasWrapper.js
+++ src/CanvasWrapper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {createStars, drawStars} from "./Stars"
 
 export default class CanvasWrapper extends React.Component{
     constructor(props){
@@ -8,8 +8,9 @@ export default class CanvasWrapper extends React.Component{
          stars:   []
         }
     }
+
     componentDidMount(){
-        this.createStars()
+        this.setState({stars: createStars(this.el)})  
         this.renderCanvas(); 
     }
     
@@ -18,63 +19,11 @@ export default class CanvasWrapper extends React.Component{
     }
 
     renderCanvas() {
-        this.drawStars()
+        drawStars(this.state.stars,this.el)
     }
 
-    createStars(){
-        const canvas = this.el;
-        const stars = [];
-        for (var i = 0; i < 500; i++) {
-            stars[i] = {
-              x: Math.random() * canvas.width,
-              y: Math.random() * canvas.height,
-              radius: Math.sqrt(Math.random() * 2),
-              alpha: 1.0,
-              decreasing: true,
-              dRatio: Math.random()*0.009
-            };
-        } 
-        this.setState({stars})
-    }
-
-    drawStars() {
-        
-        const stars = this.state.stars;
-
-        const canvas = this.el;
-        const context = canvas.getContext('2d');
-
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.save();
-        context.fillStyle = "#111"
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        for (var i = 0; i < stars.length; i++) {
-            let star = stars[i];
-            context.beginPath();
-            context.arc(star.x, star.y, star.radius, 0, 2*Math.PI);
-            context.closePath();
-            context.fillStyle = "rgba(255, 255, 255, " + star.alpha + ")";
-            star = this.updateStar(star)
-            context.fill();
-            stars[i] = star
-        }
-        context.restore();
-    }
-
-    updateStar(star){
-        if (star.decreasing){
-            star.alpha -= star.dRatio;
-            if (star.alpha < 0.1){ 
-                star.decreasing = false; 
-            }
-        } else {
-            star.alpha += star.dRatio;
-            if (star.alpha > 0.95){ 
-                star.decreasing = true;
-            }
-        }
-       return star
+    drawStars(){
+        drawStars(this.state.stars, this.el)
     }
     
     render(){
--- src/Stars.js
+++ src/Stars.js
@@ -1,97 +1,50 @@
-import React  from 'react';
+export function createStars (canvas) {
+    const stars = [];
+    for (var i = 0; i < 500; i++) {
+        stars[i] = {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: Math.sqrt(Math.random() * 2),
+            alpha: 1.0,
+            decreasing: true,
+            dRatio: Math.random()*0.009
+        };
+    } 
+    return stars
+}
 
-export default class Stars extends React.Component {
-    constructor(){
-        super();
-        this.state= {
-            stars:[],
+export function drawStars(stars, canvas) {
+    const context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.fillStyle = "#111"
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < stars.length; i++) {
+        let star = stars[i];
+        context.beginPath();
+        context.arc(star.x, star.y, star.radius, 0, 2*Math.PI);
+        context.closePath();
+        context.fillStyle = "rgba(255, 255, 255, " + star.alpha + ")";
+        star = updateStar(star)
+        context.fill();
+        stars[i] = star
+    }
+    context.restore();
+}
+
+export function updateStar(star){
+    if (star.decreasing){
+        star.alpha -= star.dRatio;
+        if (star.alpha < 0.1){ 
+            star.decreasing = false; 
         }
-        this.canvasRef = React.createRef();
-        this.createStars = this.createStars.bind(this)
-    }
-
-    get canvas() {
-        return this.canvasRef.current;
-    }
-    
-    get ctx() {
-        return this.canvas.getContext('2d');
-    }
-    
-    componentDidMount() {
-        this.renderCanvas();
-    }
-    
-      componentDidUpdate() {
-        this.renderCanvas();
-      }
-    
-      renderCanvas() {
-        const { assets, card } = this.props;
-    
-        this.drawStars()
-      }
-    
-
-    createStars(){
-        const stars = [];
-        for (var i = 0; i < 500; i++) {
-            stars[i] = {
-              x: Math.random() * this.refs.canvas.width,
-              y: Math.random() * this.refs.canvas.height,
-              radius: Math.sqrt(Math.random() * 2),
-              alpha: 1.0,
-              decreasing: true,
-              dRatio: Math.random()*0.05
-            };
+    } else {
+        star.alpha += star.dRatio;
+        if (star.alpha > 0.95){ 
+            star.decreasing = true;
         }
-        this.setState({stars})  
     }
-
-    drawStars() {
-        const stars = this.state.stars;
-
-        const canvas = this.refs.canvas;
-        const context = canvas.getContext('2d');
-
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.save();
-        context.fillStyle = "#111"
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        for (var i = 0; i < stars.length; i++) {
-            let star = stars[i];
-            context.beginPath();
-            context.arc(star.x, star.y, star.radius, 0, 2*Math.PI);
-            context.closePath();
-            context.fillStyle = "rgba(255, 255, 255, " + star.alpha + ")";
-            star = this.updateStar(star)
-            context.fill();
-            stars[i] = star
-        }
-        context.restore();
-    }
-
-    updateStar(star){
-        if (star.decreasing){
-            star.alpha -= star.dRatio;
-            if (star.alpha < 0.1){ 
-                star.decreasing = false; 
-            }
-        } else {
-            star.alpha += star.dRatio;
-            if (star.alpha > 0.95){ 
-                star.decreasing = true;
-            }
-        }
-       
-        // stars[i] = star
-         this.setState({stars})
-    }
-    render() {
-        
-        return (
-            <div/>
-        );
-    }
+   return star
 }
